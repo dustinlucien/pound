c2c2ea480960b94ba1fa24f760300cd0098b27a1
Dustin Lucien <dustin.lucien@gmail.com>
2011-07-26 13:09:31 -0400

    removed some unnecessary stuff

--- restful-backend/app.js
+++ restful-backend/app.js
@@ -3,42 +3,34 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express'),
 //RESTful routing helper
-require('express-resource')
-
-var events = require('events');
-var request = require('request');
+	, Resource = require('express-resource'),
+	, events = require('events')
+	, request = require('request');
 
 // Configuration
 var app = module.exports = express.createServer();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
-app.dynamicHelpers({ messages: require('express-messages') });
-
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
 });
 
-// Routes
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Kudos!',
-    people: people
-  });
-});
+
+//Wire up the controllers
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
