77698407af2054621fdd2a1853ae15f5f6a38188
Andrew Peace <apeace@hermione.(none)>
2011-08-16 14:41:04 -0400

    Improved Login screen to match wireframes

--- restful-backend/public/app/views/LoginPanel.js
+++ restful-backend/public/app/views/LoginPanel.js
@@ -19,18 +19,32 @@ kudos.views.LoginPanel = Ext.extend( Ext.Panel, {
 	}],
 
 	items: [
-		// a login form
-		new Ext.form.FormPanel({
+		// the body panel
+		new Ext.Panel({
+			// padding of 5px on all sides
+			bodyPadding: 5,
 			items: [
+				// the text above the login form
 				{
-					xtype: 'emailfield',
-					name: 'email',
-					placeHolder: 'email'
+					html: 'Hi there, ready to sign in?',
+					// adjust padding
+					padding: '10 0 0 5'
 				},
+				// a login form contained in a fieldset
 				{
-					xtype: 'passwordfield',
-					name: 'password',
-					placeHolder: 'password'
+					xtype: 'fieldset',
+					items: [
+						{
+							xtype: 'emailfield',
+							name: 'email',
+							placeHolder: 'email'
+						},
+						{
+							xtype: 'passwordfield',
+							name: 'password',
+							placeHolder: 'password'
+						}
+					]
 				}
 			]
 		})
