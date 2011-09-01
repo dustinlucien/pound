5cf89f0480e473159ad7af4a7813a73e3272cb76
Andrew Peace <apeace@hermione.(none)>
2011-09-01 10:17:36 -0400

    Styled activity list with mock data

--- public/app/views/ActivityPanel.js
+++ public/app/views/ActivityPanel.js
@@ -1,15 +1,16 @@
 kudos.views.ActivityPanel = Ext.extend( Ext.List, {
 
 	store: 'Kudo',
+	cls: 'kudo-list',
 
 	initComponent: function () {
 
 		var tpl = new Ext.XTemplate(
-			'<div class="kudo-item">',
+			'<div class="kudo-item {[xindex % 2 === 0 ? "even" : "odd"]}">',
 				'<tpl if="sender === kudos.data.uid">You gave </tpl>',
-				'<tpl if="sender !== kudos.data.uid">{sender_name} gave </tpl>',
+				'<tpl if="sender !== kudos.data.uid"><span class="link-text">{sender_name}</span> gave </tpl>',
 				'<tpl if="recipient === kudos.data.uid">you a kudo</tpl>',
-				'<tpl if="recipient !== kudos.data.uid"><br />{recipient_name} a kudo</tpl>',
+				'<tpl if="recipient !== kudos.data.uid"><br /><span class="link-text">{recipient_name}</span> a kudo</tpl>',
 			'</div>'
 		);
 
