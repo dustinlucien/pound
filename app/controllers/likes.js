1513ca903927a1e535a23f45c1d18eaf1932d119
Dustin Lucien <dustin.lucien@gmail.com>
2011-09-21 21:48:04 -0400

    added likes controller

---
+++ app/controllers/likes.js
@@ -0,0 +1,56 @@
+/**
+ * Dependencies
+ */
+
+var Like = require( '../models/like' );
+		
+/**
+ * Class def
+ */
+function LikeController () {
+	this.label = 'likes';
+}
+
+// extend the GenericController class
+var GenericController = require( './generic.js' );
+UserController.prototype.__proto__ = GenericController.prototype;
+
+// export the UserController class
+module.exports = LikeController;
+
+/**
+ * Class methods
+ */
+
+//GET /users  ->  index
+LikeController.prototype.index = function( req, res ) {
+	var self = this;
+
+	Like.find({}, function( err, docs ) {
+		if ( err ) {
+			self._respond( res, {}, 500 );
+		} else {
+			self._respond( res, docs );
+		}
+	});
+};
+
+//POST /likes -> create
+LikeController.prototype.create = function( req, res ) {
+	this._respond( res, null, 403 );
+};
+
+//GET /likes/:like -> show
+LikeController.prototype.show = function( req, res ) {
+	
+};
+
+//PUT /likes/:like -> update
+LikeController.prototype.update = function ( req, res ) {
+	this._respond( res, null, 403 );
+};
+
+//DELETE /likes/:like -> delete
+LikeController.prototype.destroy = function( req, res ) {
+	this._respond( res, null, 403 );
+};
